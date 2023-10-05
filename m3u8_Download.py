776c79493c4982d3b19f617382cb28c72e9e63a8
iivon <76443690+ilvon@users.noreply.github.com>
2023-10-06 01:09:52 +0800

    Update m3u8_Download.py
    modified HTTP request headers

--- m3u8_Download.py
+++ m3u8_Download.py
@@ -1,26 +1,28 @@
-import requests
-import m3u8
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-import ffmpeg
 import concurrent.futures
 import os
+import ffmpeg
+import m3u8
+import requests
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 
-url = input('m3u8 URL to be downloaded: ')
-user_agent = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+url = input('URL to be downloaded: ')
+HEADER = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'Accept-Language':'en-US, en;q=0.9, *;q=0.5',
+    'Accept-Encoding': 'gzip, br',
+    'Referer': 'https://www.google.com/'
+    }
 output_filename = 'output.mp4'
-filename_suffix = 1
+filename_suffix = 0
 
-while True:
-    if os.path.exists(output_filename):
-        output_filename = f'output({filename_suffix}).mp4'
-        filename_suffix += 1
-    else:
-        break
+while os.path.exists(output_filename):
+    filename_suffix += 1
+    output_filename = f'output({filename_suffix}).mp4'
 
 
 def proccess_segment(segsurl, urlIndex):
-    stream_encrypted = requests.get(segsurl.absolute_uri, headers=user_agent).content
+    stream_encrypted = requests.get(segsurl.absolute_uri, headers=HEADER).content
     if indexObject.keys[0]:
         stream_decrypted = cipher.decrypt(stream_encrypted)
         stream_decrypted = unpad(stream_decrypted, AES.block_size)
@@ -41,7 +43,7 @@ work_counter = 0
 print(f'\n{segment_amount} segments to be downloaded.')
 
 if indexObject.keys[0]:
-    encryption_key = requests.get(indexObject.keys[0].absolute_uri, headers=user_agent).content
+    encryption_key = requests.get(indexObject.keys[0].absolute_uri, headers=HEADER).content
     cipher = AES.new(encryption_key, AES.MODE_CBC)
 
 
